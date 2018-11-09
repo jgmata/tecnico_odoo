8e1a7e68e5f60968ec6e8322e2e0147529726c04
JOSE MATA <jose.mata@grupoabsa.com>
2018-11-09 15:57:00 +0000

    [REF] Problem with inherence, field instructor

---
+++ openacademy/models/partner.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class Partner(models.Model):
+    _inherit = 'res.partner'
+
+    instructor = fields.Boolean(default=False)
+    session_ids = fields.Many2many(
+        'openacademy.session',string="Attend Session",
+        readonly=True)
