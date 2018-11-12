41f7e11f47fe9265407ffc1a61ae9eeaf090270b
JOSE MATA <jose.mata@grupoabsa.com>
2018-11-12 17:33:18 +0000

    [REF] computed fields and default

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+import time
+
+def get_uid(self, *a):
+    return self.env.uid
 
 class Course(models.Model):
     _name = 'openacademy.course'
@@ -9,17 +13,31 @@ class Course(models.Model):
     description = fields.Text()
     responsible_id = fields.Many2one(
         'res.users',string="Responsible",
-        index=True, ondelete='set null')
-    session_ids = fields.One2many('openacademy.session','course_id')
+        index=True, ondelete='set null',
+        #default=lambda self, *a: self.env.uid
+        default=get_uid)
+    session_ids = fields.One2many('openacademy.session','course_id',string="Sessions")
 
 class Session(models.Model):
     _name = 'openacademy.session'
 
     name = fields.Char(required=True)
-    start_date = fields.Date()
+    start_date = fields.Date(default=fields.Date.today)
+    datetime_test = fields.Datetime(default=fields.Datetime.now)
     duration = fields.Float(digits=(6,2), help="Duration in days")
     seats = fields.Integer(String="Number of seats")
     instructor_id = fields.Many2one('res.partner',string='instructor', domain=['|',('instructor','=',True),('category_id.name','ilike','Teacher')])
     course_id = fields.Many2one('openacademy.course',ondelete='cascade',
                                 string='Course', required=True)
     attendee_ids = fields.Many2many('res.partner',string='Attendees')
+    taken_seats = fields.Float(compute='_taken_seats')
+    active = fields.Boolean(default=True)
+
+    @api.depends('seats','attendee_ids')
+    def _taken_seats(self):
+        #import pdb; pdb.set_trace()
+        for record in self:
+            if not record.seats:
+                record.taken_seats = 0
+            else:
+                record.taken_seats = 100.0 * len(record.attendee_ids) / record.seats
