6db1327259aa39ad06c94fac015c9cfafb3c9ae5
JOSE MATA <jose.mata@grupoabsa.com>
2018-11-23 04:11:14 +0000

    [REF] Openacademy: Add duplicate test to openacademy course

--- openacademy/tests/test_global_openacademy_course.py
+++ openacademy/tests/test_global_openacademy_course.py
@@ -30,7 +30,7 @@ class GlobalTestOpenAcademyCourse(TransactionCase):
     # Method os test startswith 'def test_*(self):'
     # Mute the error odoo.sql_db to avois it in log
     @mute_logger('odoo.sql_db')
-    def test_01_same_name_description(self):
+    def test_10_same_name_description(self):
         '''
         Test create course with same name and description.
         To test constraint of name different to description.
@@ -44,3 +44,32 @@ class GlobalTestOpenAcademyCourse(TransactionCase):
             # Create a course with same name and description to raise error
             self.create_course('JOSE', 'JOSE', None)
 
+
+
+    # Method os test startswith 'def test_*(self):'
+    # Mute the error odoo.sql_db to avois it in log
+    @mute_logger('odoo.sql_db')
+    def test_20_two_courses_same_name(self):
+        '''
+        Test to create two courses with same name.
+        To raise constraint of unique name
+        '''
+
+        new_id = self.create_course('test1','test description', None)
+        print("Nuevo Id: %s", new_id)
+
+        with self.assertRaisesRegexp(
+            IntegrityError,
+            'duplicate key value violates unique constraint "openacademy_course_name_unique"'
+            ):
+            new_id2 = self.create_course('test1','test description', None)
+            print("Nuevo Id 2: %s ", new_id2)
+
+
+    def test_15_duplicate_course(self):
+        '''
+        Test to duplicate a course and check thah work fine!
+        '''
+        course = self.env.ref('openacademy.course0')
+        course_id = course.copy()
+        print('Course Id: %s',course_id)
