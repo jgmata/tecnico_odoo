d63644ded7101e8bddf33814eeb25ebab192f50b
JOSE MATA <jose.mata@grupoabsa.com>
2018-11-26 00:15:57 +0000

    [FIX] .travis.yml: Adding travis file to run, bug fix part 23

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -60,7 +60,8 @@ class Session(models.Model):
     seats = fields.Integer(String="Number of seats")
     instructor_id = fields.Many2one(
         'res.partner', string='instructor',
-        domain=['|', ('instructor', '=', True),
+        domain=[
+            '|', ('instructor', '=', True),
             ('category_id.name', 'ilike', 'Teacher')])
     course_id = fields.Many2one('openacademy.course', ondelete='cascade',
                                 string='Course', required=True)
